cd63f0c9c5af35d681dd9bf267f5ae34ae25675f
David Sarrat González <113605621+davidsarratgonzalez@users.noreply.github.com>
2024-10-11 05:42:35 +0200

    feat: add head and tail

--- R/get.R
+++ R/get.R
@@ -5,10 +5,47 @@
 #' @param object The name of the object to retrieve for debugging.
 #' @return The requested object from all connected servers, or error messages if encountered.
 #' @export
-debug.get <- function(object, conns = NULL) {
+ds.debug.get <- function(object, conns = NULL) {
   datashield.aggregate(
     conns,
     paste0("debugGetDS(", object, ")"),
+    async = FALSE,
+    return_errors = TRUE
+  )
+}
+
+
+#' Retrieve Head of Object from DataSHIELD Server
+#'
+#' This function requests the first n rows of a dataframe from the DataSHIELD server for debugging purposes.
+#'
+#' @param object The name of the dataframe to retrieve the head for debugging.
+#' @param n The number of rows to return (default: 6)
+#' @param conns DSI connection object(s)
+#' @return The first n rows of the requested dataframe from all connected servers, or error messages if encountered.
+#' @export
+ds.debug.head <- function(object, n = 6, conns = NULL) {
+  datashield.aggregate(
+    conns,
+    paste0("debugHeadDS(", object, ", ", n, ")"),
+    async = FALSE,
+    return_errors = TRUE
+  )
+}
+
+#' Retrieve Tail of Object from DataSHIELD Server
+#'
+#' This function requests the last n rows of a dataframe from the DataSHIELD server for debugging purposes.
+#'
+#' @param object The name of the dataframe to retrieve the tail for debugging.
+#' @param n The number of rows to return (default: 6)
+#' @param conns DSI connection object(s)
+#' @return The last n rows of the requested dataframe from all connected servers, or error messages if encountered.
+#' @export
+ds.debug.tail <- function(object, n = 6, conns = NULL) {
+  datashield.aggregate(
+    conns,
+    paste0("debugTailDS(", object, ", ", n, ")"),
     async = FALSE,
     return_errors = TRUE
   )
